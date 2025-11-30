8572722259cf1232e3c0dfd604ab1d16a542dd0f
johanavalos <64297729+johanavalos@users.noreply.github.com>
2025-11-30 09:36:21 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello from Node.js server!");
 });
 
+app.get("/hola", (req, res) => {
+  res.send("Hola from Node.js server!");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
